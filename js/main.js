aae8a54074969b6a0e2a22d676937c337cd86858
Fevret <60936445+pfevrety@users.noreply.github.com>
2020-11-14 16:23:41 +0100

    Mise a jour. Fin du code global.
    Fin du code ajout de plusieurs  themes dans le menu.

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: '<eiio.dev/>'
+    }
+})
+
+var app = new Vue({
+    el:'#css',
+    data: {
+        prymarycolor: '9DFFF9'
+    }
+})
+
+document.getElementById('button1').onclick = () => {
+    // Set the variable on the document.documentElement, which is usually the <html> node
+    document.documentElement.style.setProperty('--clr-accent', '#FFC312')
+    document.body.classList.remove('nav-open');
+}
+
+document.getElementById('button2').onclick = () => {
+    // Set the variable on the document.documentElement, which is usually the <html> node
+    document.documentElement.style.setProperty('--clr-accent', '#EE5A24')
+    document.body.classList.remove('nav-open');
+}
+
+document.getElementById('button3').onclick = () => {
+    // Set the variable on the document.documentElement, which is usually the <html> node
+    document.documentElement.style.setProperty('--clr-accent', '#ED4C67')
+    document.body.classList.remove('nav-open');
+}   
+
+document.getElementById('button4').onclick = () => {
+    // Set the variable on the document.documentElement, which is usually the <html> node
+    document.documentElement.style.setProperty('--clr-accent', '#5BC0EB')
+    document.body.classList.remove('nav-open');
+}   
