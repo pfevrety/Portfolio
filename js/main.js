8f032627b7e2f961620a51d21bd5636816fe1ca6
Fevret <60936445+pfevrety@users.noreply.github.com>
2020-11-14 17:54:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,28 +27,24 @@ var app = new Vue({
 })
 
 document.getElementById('button1').onclick = () => {
-    // Set the variable on the document.documentElement, which is usually the <html> node
     document.documentElement.style.setProperty('--clr-accent', '#FFC312')
     document.body.classList.remove('nav-open');
     document.documentElement.style.overflow = 'scroll';
 }
 
 document.getElementById('button2').onclick = () => {
-    // Set the variable on the document.documentElement, which is usually the <html> node
     document.documentElement.style.setProperty('--clr-accent', '#EE5A24')
     document.body.classList.remove('nav-open');
     document.documentElement.style.overflow = 'scroll';
 }
 
 document.getElementById('button3').onclick = () => {
-    // Set the variable on the document.documentElement, which is usually the <html> node
     document.documentElement.style.setProperty('--clr-accent', '#ED4C67')
     document.body.classList.remove('nav-open');
     document.documentElement.style.overflow = 'scrol';
 }   
 
 document.getElementById('button4').onclick = () => {
-    // Set the variable on the document.documentElement, which is usually the <html> node
     document.documentElement.style.setProperty('--clr-accent', '#5BC0EB')
     document.body.classList.remove('nav-open');
     document.documentElement.style.overflow = 'scroll';
