d2a0eb0e4228eb7ba721194014f77d8679b00bd3
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-05-16 12:25:17 +0200

    Add static

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     link: [{ rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
     link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap' }]
   },
-
+  target: 'static',
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
