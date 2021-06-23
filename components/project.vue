84b1c8f4cc100aef6b344c7d601230642c9deac7
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-06-23 13:14:24 +0200

    ✏️ Update text

--- components/project.vue
+++ components/project.vue
@@ -3,8 +3,8 @@
     <h3 class="ml-2 text-3xl font-bold dark:text-white">Mes compétences</h3>
     <p class="p-2 text-2xl font-light dark:text-white">Lorem Ipsum dollor si amet</p>
   <div class=" flex flex-wrap justify-between" id="competence">
-  <ProjectCard projectName="palamazon" projectTeam="palamazon" projectLink="e" projectDescription="🖼 Palamazon front end." projectLanguage="Vue"/>
-  <ProjectCard projectName="palamazon" projectTeam="palamazon" projectLink="e" projectDescription="🖼 Palamazon front end." projectLanguage="Vue"/>
+  <ProjectCard projectName="palamazon" projectTeam="palamazon" projectLink="e" projectDescription="Lorem ipsum dolor si amfytsdhgwis hsufhghbfhjbwm uidfkjkgbgkdfwi uyhkhegdwgydgiys gdiuvgsyig iysg diyv gyisgyh f<hsgu" projectLanguage="Vue"/>
+  <ProjectCard projectName="Darwin" projectTeam="pfevrety" projectLink="e" projectDescription="A fully customizable bot with many command!" projectLanguage="Node js"/>
   <ProjectCard projectName="palamazon" projectTeam="palamazon" projectLink="e" projectDescription="🖼 Palamazon front end." projectLanguage="Vue"/>
   <ProjectCard projectName="palamazon" projectTeam="palamazon" projectLink="e" projectDescription="🖼 Palamazon front end." projectLanguage="Vue"/>
 </div>
