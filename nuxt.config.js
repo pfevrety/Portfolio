9691b7ac1f36ff0671780ad426b4f535543c83a3
Fevret <efevret@gmail.Com>
2021-10-29 11:02:29 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
+  loading: false,
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
