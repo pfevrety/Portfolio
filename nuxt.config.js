46e5d17e621e744f849e722faae88b2e96e62ead
Fevret <efevret@gmail.Com>
2021-10-30 11:07:52 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     link: [{ rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
-    link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap' }]
+    link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap' }]
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["@animxyz/core"],
