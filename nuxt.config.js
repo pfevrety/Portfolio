ba70f59381b03a15a1a7be3eb414290f7c603594
Fevret <efevret@gmail.Com>
2021-10-30 09:47:26 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,12 +21,10 @@ export default {
     link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap' }]
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  plugins: ["~/plugins/animxyz.js"],
+  css: ["@animxyz/core"],
 
   loading: false,
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
