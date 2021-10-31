d057724ad25113571e669716789768fd0e3c1120
Fevret <efevret@gmail.Com>
2021-10-31 09:57:35 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,24 +8,37 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { property: 'og:title', content: 'Eliott Portfolio' },
       { property: 'og:description', content: 'A French Developper' },
-      { property: 'og:image', content: 'https://github.com/pfevrety/Portfolio/blob/main/data/logo-big.PNG?raw=true' },
+      {
+        property: 'og:image',
+        content:
+          'https://github.com/pfevrety/Portfolio/blob/main/data/logo-big.PNG?raw=true'
+      },
       { property: 'og:url', content: 'https://eiio.dev/' },
       { property: 'twitter:title', content: 'Eliott Portfolio' },
       { property: 'twitter:description', content: 'A French Developper' },
-      { property: 'twitter:image', content: 'https://github.com/pfevrety/Portfolio/blob/main/data/logo-big.PNG?raw=true' },
-      { property: 'twitter:url', content: 'https://eiio.dev/' },
-
+      {
+        property: 'twitter:image',
+        content:
+          'https://github.com/pfevrety/Portfolio/blob/main/data/logo-big.PNG?raw=true'
+      },
+      { property: 'twitter:url', content: 'https://eiio.dev/' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     link: [{ rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
-    link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap' }]
+    link: [
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap'
+      }
+    ]
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@animxyz/core"],
+  css: ['@animxyz/core'],
 
   loading: true,
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: ["~/plugins/titl.ts", "~/plugins/animxyz.ts"],
+  plugins: ['~/plugins/titl.ts', '~/plugins/animxyz.ts'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,22 +48,27 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
+    '@nuxt/image'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxt/image'
   ],
+  image: {
+
+  },
 
   target: 'static',
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'fr',
-    },
+      lang: 'fr'
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {}
 }
