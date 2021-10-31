c5647bcfa83ada65e9050379d382c6fda420aba5
Fevret <efevret@gmail.Com>
2021-10-31 09:34:09 +0100

    Update project.vue

--- components/project.vue
+++ components/project.vue
@@ -1,45 +1,43 @@
 <template>
-    <section class="m-4 p-6" id="project">
-      <h3 class="ml-2 text-3xl font-bold dark:text-white">Projets</h3>
-      <p class="p-2 text-2xl font-light dark:text-white">
-        L'ensemble de mes projets sont disponibles sur mon
-        <a
-          class="font-medium"
-          href="https://www.github.com/pfevrety"
-        >profil github</a>
-      </p>
-      <div
-        class="flex flex-col lg:flex-row lg:flex-wrap justify-between"
-        id="competence"
-        v-if="loaded"
+  <section class="m-4 p-6" id="project">
+    <h3 class="ml-2 text-3xl font-bold dark:text-white">Projets</h3>
+    <p class="p-2 text-2xl font-light dark:text-white">
+      L'ensemble de mes projets sont disponibles sur mon
+      <a class="font-medium" href="https://www.github.com/pfevrety"
+        >profil github</a
       >
-        <ProjectCard v-for="(repo, i) in repos" :key="i" :project="repo" />
-      </div>
-    </section>
-  </template>
+    </p>
+    <div
+      class="flex flex-col lg:flex-row lg:flex-wrap justify-between"
+      id="competence"
+      v-if="loaded"
+    >
+      <ProjectCard v-for="(repo, i) in repos" :key="i" :project="repo" />
+    </div>
+  </section>
+</template>
 
 <script>
-import projectCard from "~/components/projectCard.vue";
+import projectCard from '~/components/projectCard.vue'
 
 export default {
-  name: "project",
+  name: 'project',
   component: {
     projectCard
   },
   data() {
     return {
       repos: [],
-      loaded: false
-    };
+      loaded: true
+    }
   },
 
   async mounted() {
-    const r = await fetch("https://gh-pinned-repos.egoist.sh/?username=pfevrety");
-    this.repos = await r.json();
-    this.loaded = true;
-    console.log(this.repos);
-
-  },
-};
+    await fetch('https://gh-pinned-repos.egoist.sh/?username=pfevrety')
+      .then(r => r.json())
+      .then(r => (this.repos = r))
+      .then((this.loaded = true))
+      .catch(error => console.log(error))
+  }
+}
 </script>
-
