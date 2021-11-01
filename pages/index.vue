ca51d74d9ebd72ee9aec0520f82a613492bd9641
Fevret <efevret@gmail.Com>
2021-11-01 08:44:15 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,26 +7,18 @@
 </template>
 
 <script>
-  export default {
-    head() {
-      return {
-        title: "Pfevrety",
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: 'Page principale du portfolio de pfevrety'
-          }
-        ],
-                link: [
-          {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap'
-          }
-        ]
-
-      }
+export default {
+  head() {
+    return {
+      title: 'Pfevrety',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Page principale du portfolio de pfevrety'
+        }
+      ]
     }
   }
+}
 </script>
-
