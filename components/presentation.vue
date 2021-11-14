045ff08dc9bd41b6b876010a536205fe056d3a2a
Fevret <efevret@gmail.Com>
2021-11-14 09:16:12 +0100

    Update presentation.vue

--- components/presentation.vue
+++ components/presentation.vue
@@ -1,17 +1,12 @@
 <template>
-  <section
-    class="flex items-center justify-between flex-wrap md:flex-nowrap"
-    id="presentation"
-  >
-    <div class="w-8/12">
+  <section class="flex items-center justify-between flex-wrap md:flex-nowrap" id="presentation">
+    <div class="w-full sm:w-8/12">
       <h2 class="text-5xl font-bold dark:text-white">Qui je suis</h2>
       <p
-        class="text-2xl font-light bg-green-500 w-special mt-5 mb-3 dark:text-white"
-      >
-        Developper
-      </p>
+        class="p-1 text-2xl font-light bg-green-500 w-special mt-5 mb-3 dark:text-white"
+      >Developper</p>
 
-      <div class="font-extralight dark:text-gray-400">
+      <div class="font-extralight dark:text-gray-400 mb-5">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -33,7 +28,7 @@
       quality="70"
       fit="contain"
       v-tilt
-      class="fade_animation w-56 h-56 relative lg:-left-24 shadow-2xl m-5"
+      class="fade_animation hidden sm:block w-56 h-56 relative lg:-left-24 shadow-2xl"
       src="/pfevrety.png"
       alt="pfevrety logo N design"
     />
