f867a3cc0e8e44a999a15e701e853fafc1a83d36
Fevret <efevret@gmail.Com>
2021-11-14 09:16:09 +0100

    Update project.vue

--- components/project.vue
+++ components/project.vue
@@ -1,11 +1,12 @@
 <template>
-  <section id="project">
+  <section class="mt-10" id="project">
     <h3 class="ml-2 text-3xl font-bold dark:text-white">Projets</h3>
     <p class="p-2 text-2xl font-light dark:text-white">
       L'ensemble de mes projets sont disponibles sur mon
-      <a class="font-medium" href="https://www.github.com/pfevrety"
-        >profil github</a
-      >
+      <a
+        class="font-medium"
+        href="https://www.github.com/pfevrety"
+      >profil github</a>
     </p>
     <div
       class="flex flex-col lg:flex-row lg:flex-wrap justify-between"
